ebb803c58357f004f5df31a2cad95427b4ed31dc
Marqueane <marqueane_64@hotmail.com>
2023-01-09 18:37:50 -0300

    1. Criação dos Templates 'home' e 'post_detail'. 2. Navegação entre páginas.

--- pagina1/urls.py
+++ pagina1/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'), #se não digitou nada, carregue a função/método home (view home)
+    path('posts/<int:post_id>/post_detail', views.post_detail, name='post_detail'),
 ]
--- pagina1/views.py
+++ pagina1/views.py
@@ -1,5 +1,18 @@
+#LÓGICA DE NEGÓCIO
 from django.shortcuts import render
-from django.http import HttpResponse
+from . models import Post
 
-def home(request):
-    return HttpResponse("<h1>Programa Você na Universidade!!!</h1>")
+def home(request): #Carregar a home com todos os posts catálogados no banco de dados.
+    posts = Post.objects.all()
+    context = {
+        'posts': posts
+    }
+    return render(request, 'pagina1/home.html', context)
+
+
+def post_detail(request, post_id):
+    post = Post.objects.get(pk=post_id)
+    context = {
+        'post': post
+    }
+    return render(request, 'pagina1/post_detail.html', context)
